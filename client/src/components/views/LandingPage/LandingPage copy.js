8cdf7bd4eaab83c85d130a8af53f3551b40e47aa
RakibulIslam1161062 <bsse0903@iit.du.ac.bd>
2020-09-08 23:17:09 +0600

    false commit

--- client/src/components/views/LandingPage/LandingPage copy.js
+++ client/src/components/views/LandingPage/LandingPage copy.js
@@ -43,8 +43,7 @@ return (
            
                 <YtPlayer videoId={channel.videoId} />
            
-        )   ) 
-
+        )) 
         }
     </div>
     
--- client/src/components/views/LandingPage/sections/MainDiv.js
+++ client/src/components/views/LandingPage/sections/MainDiv.js
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './channelCss.css'
+import Axios from 'axios'
+import YtPlayer from './YtPlayer'
 
 function MainDiv() {
+
+    const [videoId, setvideoId] = useState();
+    const [channelObjectArray, setchannelObjectArray] = useState([])
+    const [channelInfo, setchannelInfo] = useState([])
+
+    useEffect(() => {
+        // setvideoId("2g811Eo7K8U");
+
+        Axios.get(`http://localhost:5000/getBDNewsChannelLinks`)
+            .then((res) => {
+                // setchannelObjectArray(channelObjectArray.push(res.data))
+                // console.log(channelObjectArray);
+                // for( var i =0; i< res.data.length;i++)
+                //     setchannelInfo(channelInfo.push(res.data[i]))
+                // console.log(channelInfo)
+                setchannelInfo(res.data)
+                console.log(channelInfo)
+            })
+            .catch((err) => {
+                console.log("Problem man")
+            })
+
+    }, [])
+
+
+
+
     return (
         <div class="row" style={{ width: "100%" }}>
             <div class="column" style={{ "background-color": "#bbb" }}>
@@ -9,7 +38,7 @@ function MainDiv() {
                 <p></p>
             </div>
             <div class="column" style={{ "background-color": "#bbb" }}>
-                <div class="childFlex" style={{ "background-color": "#ccc" }}>1</div>
+                <YtPlayer class="childFlex" videoId={"RBumgq5yVrA"}  />
                 <div class="childFlex" style={{ "background-color": "#ddd" }}>2</div>
                 <div class="childFlex" style={{ "background-color": "#eee" }}>3</div>
             </div>
